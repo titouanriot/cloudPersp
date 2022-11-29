c1b5709ba51ea7d0ed47f92347565466eef9ed72
mounacqflo <101139448+mounacqflo@users.noreply.github.com>
2022-11-29 16:59:14 +0100

    Update main.rs

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,23 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+
+    let output = Command::new("echo")
+        .arg("hello world")
+        .status()
+        .expect("failed to execute");
+
+    let list_dir = Command::new("ls")
+        // command arguments
+        .args(["-l", "-a", "-R", "-t"])
+        // directory we want to display the files
+        .current_dir("src/")
+        .status()
+        .expect("ls command failed to start");
+
+    let change_permissions = Command::new("chmod")
+        .args(["-R", "777", "test/"])
+        .status()
+        .expect("chmod command failed to start");
+        
 }
