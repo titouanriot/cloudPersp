fecd4648e297e50c3a9c45131800a71fc867ee24
cabrito <cabreramat@cy-tech.fr>
2022-12-07 19:20:32 +0100

    clean code

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,5 @@
-use std::process::{Command};
+use std::process::Command;
 use reqwest;
-//use std::io::{BufWriter, Write};
 use std::str;
 
 //Exemple utilisation de la commande
@@ -13,24 +12,20 @@ fn exec_commande_shell(command : String, args : Vec<String>) -> std::io::Result<
             base_command.arg(args[i].clone());
         }
     }
-    //base_command.output().expect("error executing command");
     base_command.status().expect("error executing command");
     return Ok(base_command);      
 }
 
 #[tokio::main]
 async fn sending_request_with_result(mut result_command : String) -> std::io::Result<()> {
-
     let client = reqwest::Client::new();
     result_command = result_command.replace("\n", "n");
     result_command = result_command.replace("\r", "r");
     result_command.push_str("EOF");
     let response = client.post("http://127.0.0.1:8082/")
-    .body(result_command)   //;.strip_prefix("\n\r") .stdout    
+    .body(result_command) 
     .send()
     .await;
-    //format!("{:?}", result_command.stdout)
-    //let response = reqwest::get("http://127.0.0.1:8082").await;
     match response{
         Ok(v) => {
             match v.status() {
@@ -61,7 +56,7 @@ fn main() {
     let result_command = exec_commande_shell(String::from("ls"), vecteur);
     match result_command{
         Ok(mut v) => {
-            let mut string_result = String::from(""); //str::from_utf8(&v.output().stdout).unwrap();
+            let mut string_result = String::from("");
             for line in v.output() {
                 string_result.push_str(str::from_utf8(&line.stdout).unwrap());
             }
@@ -79,52 +74,3 @@ fn main() {
         }
     };
 }
-
-//String::from(format!("{:?}", line.stdout))
-
-
-    // let file = fs::File::open("much_beauty.png")?;
-    // let client = reqwest::Client::new();
-    // let res = client.post("http://httpbin.org/post")
-    // .headers(construct_headers())
-    // .body(file)
-    // .send()?;
-    // let result = reqwest::get("http://127.0.0.1:8082")
-    // .unwrap()
-    // .text()
-    // .await;
-
-    // let  pre_string_result = format!("{:?}", result_command);
-    // println!("{}", pre_string_result);
-    // let string_result = pre_string_result.replace("\n", "");
-    // println!("{}", pre_string_result.eq(&string_result));
-    //let mut json_result = json::stringify(string_result);
-    // let json: serde_json::Value =
-    //     serde_json::from_str(the_file).expect("JSON was not well-formatted");
-    //println!("{}", string_result);
-    //let file = File::open("temp.txt")?;
-// fn sending_request() -> std::io::Result<()>{
-//     let mut stream = TcpStream::connect("127.0.0.1:7878")?;
-//     stream.write(&[1])?;
-//     stream.read(&mut [0; 128])?;
-//     let time = time::Duration::from_millis(1000);
-//     thread::sleep(time);
-//     return Ok(());
-// }
-
-// fn sending_request_with_result(result_command : Command) -> std::io::Result<()>{
-//     let mut stream = TcpStream::connect("127.0.0.1:7878")?;
-//     stream.write(&[1])?;
-//     stream.read(&mut [0; 128])?;
-//     stream.write(result_command.output());
-//     // let time = time::Duration::from_millis(1000);
-//     // thread::sleep(time);
-//     return Ok(());
-// }
-
-// fn construct_headers() -> Headers {
-//     let mut headers = Headers::new();
-//     headers.set(UserAgent::new("reqwest"));
-//     headers.set(ContentType::png());
-//     headers
-// }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,113 +1,13 @@
-/*use reqwest::Url;
-use http::Uri;
-use std::borrow::Cow;*/
 use std::{
-    fs::{self, OpenOptions, File},
+    fs::{OpenOptions, File},
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream, SocketAddr}, time::SystemTime
+    net::{SocketAddr}
 };
-use tiny_http::{Header, Response, Request};
+use tiny_http::{Response};
 use chrono::prelude::*;
   
 // https://doc.rust-lang.org/book/ch20-01-single-threaded.html
 // telnet 127.0.0.1 8080
-
-fn handle_connection(mut stream: TcpStream) {
-    println!("Incoming connection from: {:?} \n", stream.peer_addr());
-    let mut buf_reader = BufReader::new(&mut stream);
-    let mut request_line = "".to_string();
-    let mut read_buf = Vec::new();
-    buf_reader.read_to_end(&mut read_buf);
-    let mut header_line = String::from_utf8(read_buf);
-    // loop {
-    //     buf_reader.read_line(&mut header_line);
-
-    //     // The final line is just /r/n
-    //     if header_line.len() == 2 {
-    //         break
-    //     }
-    //     header_line = "".to_string();
-    // }
-
-    // This buffer would need to be whatever size Content-Length reports
-    // buf_reader.read_exact(&mut read_buf);
-
-    // let body = String::from_utf8(read_buf.to_vec());
-    // println!("BODY: {}", body.unwrap());             TD
-    println!("{}", header_line.unwrap());
-    
- //////////TEEEEEEEESSSSSSSSTTTTTTTTTTTTTT
-    
-    // let mut buf_reader = BufReader::new(&mut stream);
-
-
-    // let mut buf = [0u8; 32];
-    // let read = buf_reader.read(&mut buf);
-    // println!("Read : {:?}", read);
-
-    // let http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
-    // println!("Request: {:#?} \n", http_request);
-    // let status_line = "HTTP/1.1 200 OK\r\n\r\n";
-    // let contents = fs::read_to_string("hello.html").unwrap();
-    // let length = contents.len();
-    // let response = format!("{status_line}Content-Length: {length}\n{contents}");
-    // println!("{}", response);
-    
-    // stream.write_all(response.as_bytes()).unwrap();
-}
-
-/*fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
-
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
-}*/
-
-fn open_connection() {
-    let addrs = [
-        SocketAddr::from(([127, 0, 0, 1], 8080)),
-        SocketAddr::from(([127, 0, 0, 1], 8081)),
-    ];
-
-    if let Ok(stream) = TcpStream::connect(&addrs[..]) {
-        println!("Connected to the server!");
-    } else {
-        println!("Couldn't connect to server...");
-    }
-}
-
-/*fn get_params_by_url(req: String) -> Vec<(String, String)> {
-    let uri = req.parse::<Uri>().unwrap();
-
-    let tmp = Url::parse(&uri.to_string()).unwrap();
-    let params = tmp.query_pairs();
-
-    let vec_params = params.collect::<Vec<(Cow<'_, str>, Cow<'_, str>)>>();
-
-    let mut res = vec![];
-    for vp in vec_params {
-        res.push((String::from(vp.0), String::from(vp.1)));
-    }
-
-    return res;
-}*/
-
 
 fn write_incoming_ip(request : Option<&SocketAddr>){
     let fp = "./beacon.txt";
@@ -180,39 +80,15 @@ fn write_logs(request : Option<&SocketAddr>){
 
 #[tokio::main]
 async fn main() {
-    // let listener = TcpListener::bind("127.0.0.1:8082").unwrap();
-    //println!("listening started, ready to accept");
-    // for stream in listener.incoming() {
-    //     let stream = stream.unwrap();
-    //     handle_connection(stream);
-    // }
     let server = tiny_http::Server::http("0.0.0.0:8082").unwrap();
     for mut request in server.incoming_requests() {
         println!("Incoming connection from: {:?} \n", request.remote_addr());
         write_incoming_ip(request.remote_addr());
         write_logs(request.remote_addr());
-        // println!("received request! method: {:?}, url: {:?}, headers: {:?}",
-        //     request.method(),
-        //     request.url(),
-        //     request.headers()
-        // );
         let mut content = String::new();
         request.as_reader().read_to_string(&mut content).unwrap();
         println!("{}", content);
         let response = Response::from_string("Response strin blabla");
         request.respond(response).unwrap();
     }
-    /*let params = [("foo", "kjj"), ("baz", "quux")];
-    let resp = Client::new()
-        .post("http://127.0.0.1:5500/test.html?foo=1&bar=2")
-        .form(&params)
-        .body("the exact body that is sent")
-        .send()
-        .await;
-
-    let request = String::from("http://127.0.0.1:5500/test.html?foo=value1&bar=value2");
-
-    let resp = get_params_by_url(request);
-
-    dbg!(resp);*/
 }
